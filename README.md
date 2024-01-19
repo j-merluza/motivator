c5b741842da71cb911b8af0beb88e8aab2fd7667
Jalen Merluza <jalen.merluza@cabelas.ca>
2024-01-19 08:38:42 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# MotivateMe
+
+Motivational quote generator. This is fun little project intended to make people smile but also, wanted to build a small project so I can test out publishing this as a package on GitHub. 
+
+Motivational quotes were downloaded from this website: https://sharpquotes.com/download-45500-famous-motivational-quotes-database-in-excel-and-pdf/#:~:text=Download%2045500%20Famous%20Motivational%20Quotes%20Database%20in%20Excel,class%20is%20running%20the%20country.%20...%20More%20items
+
+I downloaded the excel file and converted that data into a JSON file.
---
+++ motivateme.py
@@ -0,0 +1,19 @@
+import json
+import random
+
+
+class MotivateMe:
+    """
+    Selects a random motivational quote.
+
+    Attributes:
+        quote: What the fire quote was said.
+        author: Who spittin said fire quote.
+        category: Category of said fire quote.
+    """
+    def __init__(self):
+        with open("quotes.json", "r") as json_file:
+            self.data = json.load(json_file)
+        self.quote = random.choice(list(self.data))
+        self.author = self.data.get(self.quote)[0]
+        self.category = self.data.get(self.quote)[1].replace("\n", "")
